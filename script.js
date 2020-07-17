974490b72fb093eaed9d4e6101bc7fbd8dbe9db0
PoPoLabo <48846360+PoPoLabo@users.noreply.github.com>
2020-07-17 22:46:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(function() {
 
+  $('.menu-icon').click(function() {
+    $('#menu-modal').fadeIn();
+    $('.menu-icon').fadeOut();
+    $('.close-icon').fadeIn();
+  });
+  $('.close-icon').click(function() {
+    $('#menu-modal').fadeOut();
+    $('.menu-icon').fadeIn();
+    $('.close-icon').fadeOut();
+  });
+  $('.navi').click(function() {
+    $('#menu-modal').fadeOut();
+    $('.menu-icon').fadeIn();
+    $('.close-icon').fadeOut();
+  });
+
+
+
 
   $('.lesson-hover').hover(
     function() {
@@ -16,12 +34,10 @@ $(function() {
       $answer.removeClass('open');
       $answer.slideUp();
       $(this).find('span').text('+');
-
     } else {
       $answer.addClass('open');
        $answer.slideDown();
       $(this).find('span').text('-');
-
     }
   });
 
@@ -41,8 +57,18 @@ $(function() {
         }, 500);
         return false;
     });
-});
+  });
 
+$(function(){
+   $('a[href^=#]').click(function() {
+      var speed = 400;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+});
 
 
 });
